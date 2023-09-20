305dac3bbb422552f74e0b04b39cb7a4c02cdfe5
Akalanka Imesh <akalanka@arimaclanka.com>
2023-09-20 19:44:15 +0530

    minor update

--- src/components/ChatBubble.vue
+++ src/components/ChatBubble.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        <div :class="data.side == 'right' ? 'float-right bg-wa-msg-green' : 'bg-white'"
-            class=" drop-shadow-md clear-both max-w-sm mx-3 mb-1 px-3 py-2 w-max rounded-lg break-words">
-            <span class="message-span">{{ data.text }}</span><span>{{ data.time }}</span>
+        <div :class="data.side == 'right' ? 'float-right bg-wa-msg-green items-end' : 'bg-white items-start'"
+            class=" drop-shadow-md clear-both max-w-sm mx-3 mb-1 px-3 py-2 w-max rounded-lg break-words flex flex-col">
+            <span class="message-span">{{ data.text }}</span><span class="text-xs text-wa-msg-gray">{{
+                secondsFromNow(data.createdAt.seconds)
+            }}</span>
         </div>
     </div>
 </template>
 <script setup lang="ts">
-
+import moment from 'moment';
+function secondsFromNow(seconds) {
+    return moment.unix(seconds).fromNow();
+}
 defineProps({
     data: Object
 })
